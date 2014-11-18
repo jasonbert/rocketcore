80ea43850e4ed4bcee43f64632d42dde58f0e594
Jason Bert <jason.bert@goblinrockets.com>
2014-11-18 19:51:42 +0000

    Added test content; Temporarily replaced get allowed renderings

--- Source_New/Rocketcore/Rocketcore.Mvc/Pipelines/GetPlaceholderRenderings/GetDynamicKeyAllowedRenderings.cs
+++ Source_New/Rocketcore/Rocketcore.Mvc/Pipelines/GetPlaceholderRenderings/GetDynamicKeyAllowedRenderings.cs
@@ -3,6 +3,7 @@ using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Pipelines.GetPlaceholderRenderings;
+using Sitecore.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,7 +34,7 @@ namespace Rocketcore.Mvc.Pipelines.GetPlaceholderRenderings
 			}
 			else
 			{
-				return;
+				BaseProcess(args);
 			}
 
 			// Same as Sitecore.Pipelines.GetPlaceholderRenderings.GetAllowedRenderings but with fake placeholderKey
@@ -68,6 +69,79 @@ namespace Rocketcore.Mvc.Pipelines.GetPlaceholderRenderings
 					args.PlaceholderRenderings = new List<Item>();
 				}
 				args.PlaceholderRenderings.AddRange(collection);
+			}
+		}
+
+		protected virtual List<Item> GetRenderings(Item placeholderItem, out bool allowedControlsSpecified)
+		{
+			Assert.ArgumentNotNull(placeholderItem, "placeholderItem");
+			allowedControlsSpecified = false;
+			ListString str = new ListString(placeholderItem["Allowed Controls"]);
+			if (str.Count <= 0)
+			{
+				return null;
+			}
+			allowedControlsSpecified = true;
+			List<Item> list = new List<Item>();
+			foreach (string str2 in str)
+			{
+				Item item = placeholderItem.Database.GetItem(str2);
+				if (item != null)
+				{
+					list.Add(item);
+				}
+			}
+			return list;
+		}
+
+		[Obsolete("Deprecated")]
+		protected virtual List<Item> GetRenderings(string placeholderKey, string layoutDefinition, Database contentDatabase)
+		{
+			bool flag;
+			Assert.IsNotNull(placeholderKey, "placeholder");
+			Assert.IsNotNull(contentDatabase, "database");
+			Assert.IsNotNull(layoutDefinition, "layout");
+			Item placeholderItem = Client.Page.GetPlaceholderItem(placeholderKey, contentDatabase, layoutDefinition);
+			if (placeholderItem == null)
+			{
+				return null;
+			}
+			return this.GetRenderings(placeholderItem, out flag);
+		}
+
+		public void BaseProcess(GetPlaceholderRenderingsArgs args)
+		{
+			Assert.IsNotNull(args, "args");
+			Item placeholderItem = null;
+			if (ID.IsNullOrEmpty(args.DeviceId))
+			{
+				placeholderItem = Client.Page.GetPlaceholderItem(args.PlaceholderKey, args.ContentDatabase, args.LayoutDefinition);
+			}
+			else
+			{
+				using (new DeviceSwitcher(args.DeviceId, args.ContentDatabase))
+				{
+					placeholderItem = Client.Page.GetPlaceholderItem(args.PlaceholderKey, args.ContentDatabase, args.LayoutDefinition);
+				}
+			}
+			List<Item> renderings = null;
+			if (placeholderItem != null)
+			{
+				bool flag;
+				args.HasPlaceholderSettings = true;
+				renderings = this.GetRenderings(placeholderItem, out flag);
+				if (flag)
+				{
+					args.Options.ShowTree = false;
+				}
+			}
+			if (renderings != null)
+			{
+				if (args.PlaceholderRenderings == null)
+				{
+					args.PlaceholderRenderings = new List<Item>();
+				}
+				args.PlaceholderRenderings.AddRange(renderings);
 			}
 		}
 	}
